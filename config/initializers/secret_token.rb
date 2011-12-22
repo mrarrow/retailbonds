ddb707680737f8c0451d39858e382c1396e53ba3
Martin <m.r.arrowsmith@gmail.com>
2011-12-22 15:21:00 +0000

    standard rails build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Retailbonds::Application.config.secret_token = '3e0604bc74525137314e6d2c6c5a5ee6cf4756a2b7a41454a38fb524a15c9e35e444d879ee5b0fb2fb4c543c22377d9c464f802aeb8b86c8c7f9932a144b80f2'
